9046a8b2c3dcaf66000daca5ef66cb5d39c621b6
maxLundin <lundin.v.20202020@gmail.com>
2018-06-10 13:04:22 +0300

    1

---
+++ CMakeFiles/Main.cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Main.cpp.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/max/Documents/BigInteger/Main1.cpp" "/home/max/Documents/BigInteger/CMakeFiles/main.dir/Main1.cpp.o"
+  "/home/max/Documents/BigInteger/big_integer.cpp" "/home/max/Documents/BigInteger/CMakeFiles/main.dir/big_integer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
